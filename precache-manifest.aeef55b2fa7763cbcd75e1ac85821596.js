9f8b65f90d1bd8823acf39b8d59ec6b0b4ceb51e
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-02-13 17:39:16 -0700

    Updates

---
+++ precache-manifest.aeef55b2fa7763cbcd75e1ac85821596.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c87d10b4657a3d15f07517c4b1e370",
+    "url": "/devportfolio/index.html"
+  },
+  {
+    "revision": "ec12b2fe2203ea8f1ff8",
+    "url": "/devportfolio/static/css/main.a21ec3ca.chunk.css"
+  },
+  {
+    "revision": "acb637d7c6d011bfccba",
+    "url": "/devportfolio/static/js/2.ac140bbb.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/devportfolio/static/js/2.ac140bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec12b2fe2203ea8f1ff8",
+    "url": "/devportfolio/static/js/main.10b2854d.chunk.js"
+  },
+  {
+    "revision": "f9f0ae34dfd2234a4e02",
+    "url": "/devportfolio/static/js/runtime-main.a90cc0ff.js"
+  },
+  {
+    "revision": "e074373d6a8030ad63bc0ea9241f743a",
+    "url": "/devportfolio/static/media/headshot.e074373d.png"
+  },
+  {
+    "revision": "34fb9b55516f7f187d164d428e4c57fa",
+    "url": "/devportfolio/static/media/image1.34fb9b55.png"
+  }
+]);
